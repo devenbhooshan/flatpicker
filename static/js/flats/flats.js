295bc51f1611eb603d32ade9a4c63103412e5e89
Manpreet <manpreet2760@gmail.com>
2015-04-26 17:04:27 +0530

    Angular

---
+++ static/js/flats/flats.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+(function()
+{
+	var app = angular.module("flatlist", []);
+	app.controller("FlatsController", ['$scope', '$http', function($scope, $http){
+		flat=this;
+		this.flatDetails=[{title:"3BHK Apartment for Rent",location:"Old Madras Road, Bangalore",price:"21,000/ M",size:"1400",bhk:"3",furnished:"",address:"Old Madras Road, Bangalore",url:"http://ak.is2.cfcdn.com/is/p/t20/642x483/public/property-listing-images/verified/actual_size/54f53f76c391f.gif"},{title:"3BHK Apartment for Rent",location:"Old Madras Road, Bangalore",price:"21,000/ M",size:"1400",bhk:"3",furnished:"",address:"Old Madras Road, Bangalore",url:"http://ak.is2.cfcdn.com/is/p/t20/642x483/public/property-listing-images/verified/actual_size/54f53f76c391f.gif"}];
+		this.initialize=function(){
+			// HTTP get requests
+		};
+	}]);
+})();
---
+++ static/js/lp/lp.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+(function()
+{
+	var app = angular.module("lp", []);
+	app.controller("LandingpageController", ['$scope', '$http', function($scope, $http){
+		lp=this;
+		this.company=["Company","Google", "Microsoft","TCS","Wipro", "Accenture"];
+		this.companySelected=this.company[0];
+		this.city=["City","Bangalore"];
+		this.citySelected=this.city[0];
+		this.area=["Area", "All"];
+		this.areaSelected=this.area[0];
+		this.initialize=function(){
+			// HTTP get requests
+		};
+		this.submitReq=function(){
+			$http.post('/flats/').success(function(data){
+			});
+		};
+	}]);
+})();
